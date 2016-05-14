91b8ab3b9b1144f94f3a964cc6bc9688ff7fcf09
Elton Schivei Costa <costa@elton.schivei.nom.br>
2016-05-14 01:56:29 -0300

    Create setup_dbs.sh

---
+++ setup_dbs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Creating directory"
+SQLITEDIR=/tmp/sqlitedbs
+rm -rf $SQLITEDIR
+if [ -a $SQLITEDIR ]
+then
+	echo "Failed to remove $SQLITEDIR"
+	exit 1
+fi
+mkdir -p $SQLITEDIR
+cd $SQLITEDIR
+echo "Removing old DBs"
+rm -f test live
+echo "Creating DBs"
+echo 'create table t1(c1 text);' | sqlite test
+echo 'create table t1(c1 text);' | sqlite live
+echo "Inserting data"
+echo 'insert into t1 values ("test");' | sqlite test
+echo 'insert into t1 values ("live");' | sqlite live
+cd - > /dev/null 2>&1 
+echo "All done OK"
